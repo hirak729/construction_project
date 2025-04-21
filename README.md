371fbf8421caf9adc4fdd6662114dc30873c1b0d
hirak729 <hirakjyoti1233@gmail.com>
2025-04-21 18:13:43 +0530

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+#This is cunstruction web application 
+#Here i use react and bootstrap for frontend, laravel for backend and my sql database
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className='btn btn-success'>click me</button>
+    </>
+  )
+}
+
+export default App
